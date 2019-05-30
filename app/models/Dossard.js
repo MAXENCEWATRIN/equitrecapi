6a4e23d87b766a501747d6bd211632086fb7b4eb
Thutur39 <antonio.arthur2897@gmail.com>
2019-05-30 09:41:55 +0200

    Define table Dossard and Niveau

---
+++ app/models/Dossard.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+
+module.exports = sequelize.define("dossard", {
+    id: {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    numero: {
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        unique: true,
+    }
+});

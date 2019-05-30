f004711d8eda592d7fdf466078e558311b48845a
Thutur39 <antonio.arthur2897@gmail.com>
2019-05-30 10:59:05 +0200

    Edit utilisateur.nom to utilisateur.identifiant

--- app/models/Utilisateur.js
+++ app/models/Utilisateur.js
@@ -1,19 +1,19 @@
 const Sequelize = require("sequelize");
 
-module.exports = sequelize.define("utilisateur",{
-    id : {
-        type : Sequelize.INTEGER(11),
+module.exports = sequelize.define("utilisateur", {
+    id: {
+        type: Sequelize.INTEGER(11),
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
     },
-    nom : {
-        type : Sequelize.STRING(255),
+    identifiant: {
+        type: Sequelize.STRING(255),
         allowNull: false,
-        unique : true,
+        unique: true,
     },
-    motdepasse : {
-        type : Sequelize.STRING(255),
+    motdepasse: {
+        type: Sequelize.STRING(255),
         allowNull: false
     },
     freezeTableName: true
